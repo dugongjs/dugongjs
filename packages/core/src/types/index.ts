d653d13d2c712693188f7d5d514fca867ada8b9b
Erik Lysne <contact@eriklysne.com>
2025-04-28 12:42:58 +0200

    :sparkles: (core) add additional type exports

--- packages/core/src/types/index.ts
+++ packages/core/src/types/index.ts
@@ -1,1 +1,3 @@
+export type { Constructor } from "./constructor.type.js";
+export type { RemoveAbstract } from "./remove-abstract.type.js";
 export type { SerializableObject } from "./serializable-object.type.js";
