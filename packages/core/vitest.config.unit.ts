868277b4781f3186f255dbc5c98db053da919f7c
Erik Lysne <contact@eriklysne.com>
2025-05-06 16:50:38 +0200

    :wrench: (core) update test config

--- packages/core/vitest.config.unit.ts
+++ packages/core/vitest.config.unit.ts
@@ -1,12 +1,13 @@
+import { join } from "path";
 import swc from "unplugin-swc";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
     test: {
         globals: true,
-        root: "./",
-        reporters: "verbose",
-        setupFiles: ["./test/setup/reflect-metadata.ts"]
+        root: __dirname,
+        reporters: ["verbose"],
+        setupFiles: [join(__dirname, "test", "setup", "reflect-metadata.ts")]
     },
     plugins: [swc.vite()]
 });
