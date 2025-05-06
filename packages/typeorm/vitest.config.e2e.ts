305182165a357f6e0f313c6bce1d12ba6c78b0c4
Erik Lysne <contact@eriklysne.com>
2025-05-06 16:51:01 +0200

    :wrench: (typeorm) update test config

--- packages/typeorm/vitest.config.e2e.ts
+++ packages/typeorm/vitest.config.e2e.ts
@@ -1,20 +1,19 @@
 import { config } from "dotenv";
+import { join } from "path";
 import swc from "unplugin-swc";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
     test: {
         globals: true,
-        root: "./test/e2e",
+        root: join(__dirname, "test", "e2e"),
         hookTimeout: 60000,
         reporters: ["default"],
+        globalSetup: [join("setup", "global-setup", "setup-postgresql-testcontainer.ts")],
+        setupFiles: [join("setup", "setup", "reflect-metadata.ts"), join("setup", "setup", "data-source.ts")],
         fileParallelism: false,
-        globalSetup: ["./setup/global-setup/setup-postgresql-testcontainer.ts"],
-        setupFiles: ["./setup/setup/reflect-metadata.ts", "./setup/setup/data-source.ts"],
-        env: {
-            ...config({ path: "./test/e2e/.env.e2e" }).parsed
-        },
-        disableConsoleIntercept: true
+        disableConsoleIntercept: true,
+        env: { ...config({ path: join(__dirname, ".env.e2e") }).parsed }
     },
     plugins: [
         swc.vite({
