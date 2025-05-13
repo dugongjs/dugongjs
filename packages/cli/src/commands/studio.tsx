d75373a70de78456d5e0209a1f877f2696368f5e
Erik Lysne <contact@eriklysne.com>
2025-05-13 11:34:29 +0200

    :bug: (cli) fix import error

--- packages/cli/src/commands/studio.tsx
+++ packages/cli/src/commands/studio.tsx
@@ -1,7 +1,7 @@
 import { Command } from "commander";
 import { withFullScreen } from "fullscreen-ink";
 import React from "react";
-import { MainView } from "../studio/main-view.jsx";
+import { MainView } from "../studio/main-view.js";
 
 const studio = new Command("studio").description("Launch the interactive studio").action(() => {
     withFullScreen(<MainView />).start();
