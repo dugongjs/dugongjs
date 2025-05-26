69a98042116099936e70c917bf733db3e2396619
Erik Lysne <contact@eriklysne.com>
2025-05-26 09:54:50 +0200

    :bug: (add missing export of AggregateMessageConsumerService)

--- packages/nestjs/src/index.ts
+++ packages/nestjs/src/index.ts
@@ -1,3 +1,4 @@
+export { AggregateMessageConsumerService } from "./aggregate-message-consumer/aggregate-message-consumer.service.js";
 export { AggregateMessageProducerService } from "./aggregate-message-producer/aggregate-message-producer.service.js";
 export { AggregateQueryService } from "./aggregate-query/aggregate-query.service.js";
 export { InjectConsumedMessageRepository } from "./decorators/inject-comsumed-message-repository.decorator.js";
