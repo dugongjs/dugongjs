e84a10be278dcd0d225001f46effcee3d29676c6
Erik Lysne <contact@eriklysne.com>
2025-08-29 13:15:03 +0200

    :recycle: (nestjs) replace abstract types

--- packages/nestjs/src/query-model-projection-consumer/i-query-model-projection-handler.ts
+++ packages/nestjs/src/query-model-projection-consumer/i-query-model-projection-handler.ts
@@ -1,6 +1,6 @@
-import type { AbstractAggregateRoot, RemoveAbstract, TransactionContext } from "@dugongjs/core";
+import type { AggregateRoot, TransactionContext } from "@dugongjs/core";
 
-export interface IQueryModelProjectionHandler<TAggregateRoot extends RemoveAbstract<typeof AbstractAggregateRoot>> {
+export interface IQueryModelProjectionHandler<TAggregateRoot extends AggregateRoot> {
     getAggregateClass(): TAggregateRoot;
     updateQueryModel(transactionContext: TransactionContext, aggregate: InstanceType<TAggregateRoot>): Promise<void>;
     deleteQueryModel(transactionContext: TransactionContext, aggregateId: string): Promise<void>;
